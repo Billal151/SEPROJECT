c8f1e245fa68f1390f6428709190570986fd20ae
Billal151 <blllmabuza151@gmail.com>
2014-10-13 17:04:42 +0200

    Create head.h

---
+++ head.h
@@ -0,0 +1,9 @@
+#include <string>
+
+using namespace std;
+
+void quickSort(int[], int n, string k);
+int compare1(const void *a, const void *b);
+int compare2(const void *a, const void *b);
+int main();
+int cnt();
